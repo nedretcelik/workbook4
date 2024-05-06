82a6ca02e4bb19c5ee90ff395aa927d0579f0c78
nedretcelik <nedretcelik97@gmail.com>
2024-05-06 10:22:05 -0400

    object demo

---
+++ demos/javascript-objects.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let employee = {
+  employeeId: "1",
+  name: "Ezra",
+  jobTitle: "Theater Teacher",
+  payRate: 38.46,
+};
+
+console.log(employee.name,employee.jobTitle, employee.payRate);
+
