216fe0a19dbd8d9f87e1f0d84c22689c39846eb7
nedretcelik <nedretcelik97@gmail.com>
2024-05-07 10:05:03 -0400

    while exercises

---
+++ exercises/loops/do-while.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let love = "I love loops";
+let count = 1;
+
+do {
+  console.log(count + " " + love);
+  count++;
+} while (count < 8);
---
+++ exercises/loops/while-loops1.js
@@ -0,0 +1,13 @@
+"use strict";
+let love = "I love loops"
+
+let count = 1;
+
+while (count<8) {
+  
+    console.log(count +" "+love);
+    count++;
+}
+
+
+
