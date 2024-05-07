ae514174c61588894e3c5c96db23b45f1cceb139
nedretcelik <nedretcelik97@gmail.com>
2024-05-07 15:04:35 -0400

    student average

---
+++ exercises/array/student-average.js
@@ -0,0 +1,25 @@
+let students = [
+  { name: "Zephaniah", scores: [100, 96, 99, 92] },
+  { name: "Pursalane", scores: [92, 89, 96, 100, 94] },
+  { name: "Siddalee", scores: [86, 72, 92] },
+  { name: "Ian", scores: [98, 84, 89, 100, 100, 76] },
+  { name: "Elisha", scores: [89, 100] },
+  { name: "Ezra", scores: [100, 99, 100, 87] },
+];
+
+
+
+
+for(let i = 0; i<students.length; i++) {
+     console.log(students[i].name);
+     let scoreTotal = 0;
+    for(let j=0; j<students[i].scores.length; j++) {
+        console.log(students[i].scores[j])
+        scoreTotal += students[i].scores[j]
+  
+    }
+    
+    let avg = scoreTotal/ students[i].scores.length;
+  console.log(`${students[i].name}'s scores avarage is ${avg}`)
+}
+
